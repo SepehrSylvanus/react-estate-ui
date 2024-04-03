992dca7d4c898b461ae9253747fde66f38bc9ca3
Sylvanus <sepehr.haghdoust@gmail.com>
2024-04-03 15:28:57 +0330

    responsive navbar front

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import "./navbar.scss";
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="" />
+          <span>Sylvanus State</span>
+        </a>
+
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+        <a href="/">Sign in</a>
+        <a href="/" className="register">
+          Sign out
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
